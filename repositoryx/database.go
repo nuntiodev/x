efc8ae96b541f02542bba7c67d4d97de75aa4f35
Oscar A. B. Orellana <oscar@softcorp.io>
2022-04-15 18:26:49 +0200

    updated

---
+++ repositoryx/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"context"
+
+	"github.com/nuntio-io/x/repositoryx/mongo_client"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
+)
+
+type Database struct {
+	zapLog *zap.Logger
+}
+
+func CreateDatabase(zapLog *zap.Logger) (*Database, error) {
+	return &Database{
+		zapLog: zapLog,
+	}, nil
+}
+
+func (db *Database) CreateMongoClient(ctx context.Context) (*mongo.Client, error) {
+	return mongo_client.CreateMongoClient(ctx, db.zapLog)
+}
---
+++ repositoryx/mongo_client/mongo_client.go
@@ -0,0 +1,77 @@
+package mongo_client
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
+)
+
+var (
+	mongoUri          = ""
+	mongoUser         = ""
+	mongoUserPassword = ""
+	mongoHost         = ""
+)
+
+func initializeMongoClient() error {
+	var ok bool
+	mongoUri, ok = os.LookupEnv("MONGO_URI")
+	if ok && mongoUri != "" {
+		return nil
+	}
+	mongoUser, ok = os.LookupEnv("MONGO_DB_USER")
+	if !ok {
+		return errors.New("missing required MONGO_DB_USER")
+	}
+	mongoUserPassword, ok = os.LookupEnv("MONGO_DB_USER_PASSWORD")
+	if !ok {
+		return errors.New("missing required MONGO_DB_USER_PASSWORD")
+	}
+	mongoHost, ok = os.LookupEnv("MONGO_DB_HOST")
+	if !ok || mongoHost == "" {
+		return errors.New("missing required MONGO_DB_HOST")
+	}
+	return nil
+}
+
+func CreateMongoClient(ctx context.Context, zapLog *zap.Logger) (*mongo.Client, error) {
+	zapLog.Info("trying to create mongo client...")
+	if err := initializeMongoClient(); err != nil {
+		return nil, err
+	}
+	// either specify uri or user, host and password
+	if mongoUri == "" {
+		mongoUri = fmt.Sprintf("mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority", mongoUser, mongoUserPassword, mongoHost)
+	}
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		mongoUri,
+	))
+	if err != nil {
+		zapLog.Error(fmt.Sprintf("could not connect to mongo with uri %s", mongoUri))
+		return nil, err
+	}
+	if err := PerformMongHealthCheck(ctx, client); err != nil {
+		return nil, err
+	}
+	zapLog.Info("mongo client created...")
+	return client, nil
+}
+
+func PerformMongHealthCheck(ctx context.Context, client *mongo.Client) error {
+	if err := checkMongoConnection(ctx, client); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkMongoConnection(ctx context.Context, client *mongo.Client) error {
+	if err := client.Ping(ctx, nil); err != nil {
+		return err
+	}
+	return nil
+}
