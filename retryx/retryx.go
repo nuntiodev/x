3029fed1e80e55035bb5d3716c80a644c3b7911a
Oscar A. B. Orellana <oscar@softcorp.io>
2022-06-26 12:18:02 +0200

    updated

--- retryx/retryx.go
+++ retryx/retryx.go
@@ -9,6 +9,7 @@ func Retry(amount int, dur time.Duration, x func() error) error {
 	if amount <= 0 {
 		return errors.New("amount must be larger than 0")
 	}
+
 	var err error
 	for i := 0; i < amount; i++ {
 		if err = x(); err != nil {
