ce863b72412b8c71284710598f62af319bd4af17
Oscar A. B. Orellana <oscar@softcorp.io>
2022-06-28 16:54:39 +0200

    updated

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/MicahParks/keyfunc v1.1.0 // indirect
 	github.com/Microsoft/go-winio v0.5.1 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
@@ -22,15 +23,18 @@ require (
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
+	github.com/nuntiodev/hera-proto v0.2.84 // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/opencontainers/runc v1.0.2 // indirect
+	github.com/ory/hydra-client-go v1.10.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
@@ -49,6 +53,6 @@ require (
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ hydrax/hydra_authorize.go
@@ -0,0 +1,137 @@
+package cloud_authorize
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/MicahParks/keyfunc"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/nuntiodev/hera-proto/go_hera"
+	"github.com/ory/hydra-client-go/client"
+	"github.com/ory/hydra-client-go/client/public"
+	"go.uber.org/zap"
+)
+
+var (
+	hydraPublicUrl = ""
+	nuntioCloudId  = ""
+)
+
+type HeraResponse struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+}
+
+type UserInfoResponse struct {
+	Aud  []string     `json:"aud"`
+	Iss  string       `json:"iss"`
+	User HeraResponse `json:"user"`
+}
+
+type Authorize interface {
+	VerifyAndDecodeToken(token string) (*go_hera.User, error)
+}
+
+type defaultAuth struct {
+	zapLog      *zap.Logger
+	hydraClient *client.OryHydra
+	jwks        *keyfunc.JWKS
+}
+
+func initializeAuth() error {
+	var ok bool
+	hydraPublicUrl, ok = os.LookupEnv("HYDRA_PUBLIC_URL")
+	if !ok || hydraPublicUrl == "" {
+		return errors.New("missing required HYDRA_PUBLIC_URL")
+	}
+	nuntioCloudId, ok = os.LookupEnv("NUNTIO_CLOUD_ID")
+	if !ok || nuntioCloudId == "" {
+		return errors.New("missing required NUNTIO_CLOUD_ID")
+	}
+	return nil
+}
+
+func New(ctx context.Context, zapLog *zap.Logger) (Authorize, error) {
+	zapLog.Info("creating auth module")
+	if err := initializeAuth(); err != nil {
+		return nil, err
+	}
+	publicUrl, err := url.Parse(hydraPublicUrl)
+	if err != nil {
+		return nil, err
+	}
+	hydraClient := client.NewHTTPClientWithConfig(
+		nil,
+		&client.TransportConfig{
+			Schemes:  []string{publicUrl.Scheme},
+			Host:     publicUrl.Host,
+			BasePath: publicUrl.Path,
+		},
+	)
+	var hydraErr error
+	var isReady *public.IsInstanceReadyOK
+	for i := 0; i < 3; i++ {
+		time.Sleep(time.Second * 1)
+		isReady, hydraErr = hydraClient.Public.IsInstanceReady(&public.IsInstanceReadyParams{
+			Context: ctx,
+		})
+		if hydraErr != nil {
+		} else if isReady.Payload.Status != "ok" {
+			hydraErr = errors.New("service is not ready")
+		} else {
+			break
+		}
+	}
+	// CreateProject the keyfunc options. Refresh the JWKS every hour and log errors.
+	refreshInterval := time.Hour
+	options := keyfunc.Options{
+		RefreshInterval: refreshInterval,
+		RefreshTimeout:  time.Second * 10,
+		RefreshErrorHandler: func(err error) {
+			log.Printf("There was an error with the jwt.KeyFunc\nError: %s", err.Error())
+		},
+	}
+	if hydraErr != nil {
+		return nil, hydraErr
+	}
+	// CreateProject the JWKS from the resource at the given URL.
+	jwks, err := keyfunc.Get(hydraPublicUrl+"/.well-known/jwks.json", options)
+	if err != nil {
+		return nil, err
+	}
+	defaultAuth := &defaultAuth{
+		zapLog:      zapLog,
+		hydraClient: hydraClient,
+		jwks:        jwks,
+	}
+	return defaultAuth, nil
+}
+
+func (da *defaultAuth) VerifyAndDecodeToken(accessToken string) (*go_hera.User, error) {
+	if accessToken == "" {
+		return nil, errors.New("access token is empty")
+	}
+	token, err := jwt.Parse(accessToken, da.jwks.Keyfunc)
+	if err != nil {
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, errors.New("token is not valid")
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, errors.New("could not extract claims")
+	}
+	userId, ok := claims["sub"].(string)
+	if !ok {
+		return nil, errors.New("userid is empty")
+	}
+	return &go_hera.User{
+		Id: userId,
+		//Email: userInfoResp.User.Email,
+	}, nil
+}
